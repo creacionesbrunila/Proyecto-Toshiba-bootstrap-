5bbb64b2430aec366b3d49f188890e3f6100321f
brunila <brunila110@gmail.com>
2023-10-21 12:17:40 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/* Toggle del Menú Mobile */
+const btnToggler = document.getElementById("btnToggler");
+const btnMenu = document.getElementById("btnMenu");
+const btnClose =document.getElementById("btnClose");
+btnToggler.addEventListener('click', ()=>{
+  btnMenu.classList.toggle("d-none");
+  btnClose.classList.toggle("d-none");
+})
